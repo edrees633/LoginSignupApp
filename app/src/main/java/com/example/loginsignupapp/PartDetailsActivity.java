ba887dd06c8836d43466022caed6b9e7d1646780
edrees abu khames <edreesabukha@gmail.com>
2022-02-08 10:11:29 +0200

    project upgraded 8.2.2022

--- app/src/main/java/com/example/loginsignupapp/PartDetailsActivity.java
+++ app/src/main/java/com/example/loginsignupapp/PartDetailsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PartDetailsActivity extends AppCompatActivity {
+public class  PartDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
