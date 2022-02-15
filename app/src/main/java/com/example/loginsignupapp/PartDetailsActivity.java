4e90164562a77f92c9dffc457de1d37dbf2acc68
edrees abu khames <edreesabukha@gmail.com>
2022-02-15 10:15:13 +0200

    project upgraded 15.2.2022 coded detailsPart

--- app/src/main/java/com/example/loginsignupapp/PartDetailsActivity.java
+++ app/src/main/java/com/example/loginsignupapp/PartDetailsActivity.java
@@ -2,13 +2,21 @@ package com.example.loginsignupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class  PartDetailsActivity extends AppCompatActivity {
+
+    private TextView tvName, tvDescription,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_details);
+
+        Intent i=this.getIntent();
+        Part part = (Part) i.getSerializableExtra("part");
+
     }
 }
